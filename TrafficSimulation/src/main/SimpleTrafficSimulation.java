0657db5c04503fbddf65e6d422846793340cdc0d
Derreck <mansheim23@hotmail.com>
2016-10-30 15:27:39 -0500

    Initial commit

---
+++ TrafficSimulation/src/main/SimpleTrafficSimulation.java
@@ -0,0 +1,19 @@
+package main;
+
+import model.SimpleModel;
+import model.swing.SwingAnimatorBuilder;
+
+/**
+ * A static class to demonstrate the visualization aspect of
+ * simulation.
+ */
+public class SimpleTrafficSimulation {
+	private SimpleTrafficSimulation() {}
+	public static void main(String[] args) {
+		SimpleModel m = new SimpleModel(new SwingAnimatorBuilder());
+		m.run(500);
+		m.dispose();
+		System.exit(0);
+	}
+}
+
---
+++ TrafficSimulation/src/main/TrafficSimulation.java
@@ -0,0 +1,43 @@
+package main;
+
+import model.Model;
+import model.swing.SwingAnimatorBuilder;
+import model.text.TextAnimatorBuilder;
+
+/**
+ * A static class to demonstrate the visualization aspect of
+ * simulation.
+ */
+public class TrafficSimulation{
+	private TrafficSimulation() {}
+	public static void main(String[] args) {
+		{
+			Model m = new Model(new TextAnimatorBuilder(), 0, 1);
+			m.run(10);
+			m.dispose();
+		}
+		{
+			Model m = new Model(new SwingAnimatorBuilder(), 0, 1);
+			m.run(10);
+			m.dispose();
+		}
+		{
+			Model m = new Model(new TextAnimatorBuilder(), 1, 1);
+			m.run(10);
+			m.dispose();
+		}
+		{
+			Model m = new Model(new SwingAnimatorBuilder(), 1, 1);
+			m.run(10);
+			m.dispose();
+		}
+		{
+			Model m = new Model(new SwingAnimatorBuilder(), 2, 3);
+			m.run(500);
+			m.run(500);
+			m.dispose();
+		}
+		System.exit(0);
+	}
+}
+
