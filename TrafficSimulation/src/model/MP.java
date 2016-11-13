b78d19280c46ee121f37cbaffd80984eb0b9f84c
derreck <mansheim23@hotmail.com>
2016-11-12 23:26:39 -0600

    Set up fields for simulation run time and time elapsed between steps

--- TrafficSimulation/src/model/MP.java
+++ TrafficSimulation/src/model/MP.java
@@ -7,9 +7,17 @@ public class MP {
 	private MP() {}
 	/** Length of cars, in meters */
 	public static double carLength = 10;
+	
 	/** Length of roads, in meters */
 	public static double roadLength = 200;
+	
 	/** Maximum car velocity, in meters/second */
 	public static double maxVelocity = 6;
+	
+	private double simTimeStep;  //How much model time elapses between each simulation step
+	
+	private int simRunTime;  	 //the length of the simulation in model seconds
+	
+	
 }
 
