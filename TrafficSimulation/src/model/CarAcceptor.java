0df43f454a13f4028732854d298bb93ab44964cc
derreck <mansheim23@hotmail.com>
2016-11-14 17:37:13 -0600

    added necessary getter and setter methods to CarAcceptor Interface

--- TrafficSimulation/src/model/CarAcceptor.java
+++ TrafficSimulation/src/model/CarAcceptor.java
@@ -1,5 +1,14 @@
 package model;
 
+import java.util.Set;
+import model.Data.Orientation;
+
 public interface CarAcceptor {
 	public boolean accept(Car c, Double firstPosition);
+	public Double distanceToObstacle(Double fromPosition, Orientation orientation);
+	public Double getLastPosition();
+	public RoadEnd getNextRoad(Orientation orientation);
+	public void setNextRoad(RoadEnd road);
+	public boolean remove(Car car);
+	public Set<Car> getCars();	
 }
