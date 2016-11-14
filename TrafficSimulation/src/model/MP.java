15d572b2da0e9550cafc165e764cabe8fe085d21
derreck <mansheim23@hotmail.com>
2016-11-13 22:11:45 -0600

    Set up getter methods and fields in MP.java for Car constructor

--- TrafficSimulation/src/model/MP.java
+++ TrafficSimulation/src/model/MP.java
@@ -14,10 +14,52 @@ public class MP {
 	/** Maximum car velocity, in meters/second */
 	public static double maxVelocity = 6;
 	
-	private double simTimeStep;  //How much model time elapses between each simulation step
+
+	private double carLengthMin; 		// the minimum length of a car (in meters)
+	private double carLengthMax; 		// the maximum length of a car (in meters)
+	private double maxVelocityMin;		// the minimum top car velocity (in meters/second)
+	private double maxVelocityMax;		// the maximum top car velocity (in meters/second)
+	private double stopDistanceMin;		// the minimum distance away from others when car must stop (in meters) 
+	private double stopDistanceMax;		// the maximum distance away from others when car must stop (in meters)
+	private double brakeDistanceMin; 	// the minimum distance away from others when car must start braking (in meters) 
+	private double brakeDistanceMax;	// the maximum distance away from others when car must start braking (in meters)
+
 	
-	private int simRunTime;  	 //the length of the simulation in model seconds
+	public double getCarLengthMin() {
+		return carLengthMin;
+	}
 	
+	public double getCarLengthMax() {
+		return carLengthMax;
+	}
+	
+	public double getMaxVelocityMin() {
+		return maxVelocityMin;
+	}
+	
+	public double getMaxVelocityMax() {
+		return maxVelocityMax;
+	}
+	
+	public double getStopDistanceMin() {
+		return stopDistanceMin;
+	}
+	
+	public double getStopDistanceMax() {
+		return stopDistanceMax;
+	}
+	
+	public double getBrakeDistanceMin() {
+		return brakeDistanceMin;
+	}
+	
+	public double getBrakeDistanceMax() {
+		return brakeDistanceMax;
+	}
+	
+	public double createRandom(double min, double max){
+		return min+(int)(Math.random() * ((max - min) + 1));
+	}
 	
 }
 
