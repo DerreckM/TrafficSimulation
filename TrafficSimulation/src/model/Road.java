efedc9f201f0d3922d1a633af097b6f2d79c633c
derreck <mansheim23@hotmail.com>
2016-11-14 17:42:24 -0600

    Placed method stubs in Road.java

--- TrafficSimulation/src/model/Road.java
+++ TrafficSimulation/src/model/Road.java
@@ -2,32 +2,57 @@ package model;
 
 import java.util.List;
 import java.util.ArrayList;
+import java.util.Set;
+
+import model.Data.Orientation;
 
 /**
  * A road holds cars.
  */
 public class Road implements CarAcceptor {
 	
-	CarAcceptor nextAcceptor;
 	
-	Road() {   
-		nextAcceptor = null;
-	}  // Created only by this package
-	
-	
-	private List<Car> cars = new ArrayList<Car>();
-
-	public void accept(Car d) {
-		if (d == null) { throw new IllegalArgumentException(); }
-		cars.add(d);
-	}
-	public List<Car> getCars() {
-		return cars;
-	}
 	
 	@Override
 	public boolean accept(Car c, Double firstPosition) {
 		// TODO Auto-generated method stub
 		return false;
+	}
+
+	@Override
+	public Double distanceToObstacle(Double fromPosition,
+			Orientation orientation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public Double getLastPosition() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public RoadEnd getNextRoad(Orientation orientation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void setNextRoad(RoadEnd road) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean remove(Car car) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public Set<Car> getCars() {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
