555f83a0bac22ef40a70f40b02cc891b324371bb
derreck <mansheim23@hotmail.com>
2016-11-17 21:43:42 -0600

    Added fields to RoadEnd interface

--- TrafficSimulation/src/model/RoadEnd.java
+++ TrafficSimulation/src/model/RoadEnd.java
@@ -1,5 +1,13 @@
 package model;
 
+import model.Data.Orientation;
+
 public interface RoadEnd {
+
+	double getLastPosition();
+	CarAcceptor getNextRoad(Orientation orientation);
+	void remove(Car car);
+	void accept(Car car, double d);
+	double distanceToObstacle(double firstPosition, Orientation orientation);
 
 }
