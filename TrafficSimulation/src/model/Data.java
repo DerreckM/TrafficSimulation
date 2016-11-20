1f9ccaac315793e60ec36657008feca34afbdcc3
derreck <mansheim23@hotmail.com>
2016-11-20 02:24:43 -0600

    created light state and methods to create objects in Data

--- TrafficSimulation/src/model/Data.java
+++ TrafficSimulation/src/model/Data.java
@@ -5,5 +5,37 @@ public class Data {
 	public enum Orientation {
 		NS, EW
 	}
+	
+	public enum LightState {
+		NSGREEN, EWGREEN, NSYELLOW, EWYELLOW
+	}
 
+	public Data() {
+		
+	}
+	
+	static public final Car makeCar(Orientation orientation) {
+		return new Car(orientation);
+	}
+	
+	static public final Road makeRoad() {
+		return new Road();
+	}
+	
+	static public final RoadEnd makeIntersection() {
+		return new IntersectionObject();
+	}
+	
+	static public final RoadEnd makeSink() {
+		return new Sink();
+	}
+	
+	static public final LightObject makeLight() {
+		return new LightObject();
+	}
+	
+	static public final CarSource makeSource(Orientation orientation) {
+		return new CarSourceObject(orientation);
+	}
+	
 }
