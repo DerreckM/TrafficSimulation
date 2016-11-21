72481deb9bd10ebf484a3d68b6d91e8c678ab739
derreck <mansheim23@hotmail.com>
2016-11-21 15:08:28 -0600

    Fleshed out run, toString, enqueue methods and Agent dequeue

--- TrafficSimulation/src/timeServer/TimeServerLinked.java
+++ TrafficSimulation/src/timeServer/TimeServerLinked.java
@@ -20,11 +20,6 @@ public final class TimeServerLinked extends Observable implements TimeServer {
 	private int size;
 	private Node head;
 
-	/*
-	 * Invariant: _head != null
-	 * Invariant: _head.agent == null
-	 * Invariant: (_size == 0) iff (_head.next == null)
-	 */
 	public TimeServerLinked() {
 		size = 0;
 		head = new Node(0, null, null);
@@ -32,19 +27,63 @@ public final class TimeServerLinked extends Observable implements TimeServer {
 
 	@Override
 	public double currentTime() {
-		// TODO Auto-generated method stub
-		return 0;
+		return currentTime;
+	}
+	
+	public String toString() {
+		StringBuilder stringbuilder = new StringBuilder("[");
+		Node node = head.next;
+		String separate = "";
+		while (node != null) {
+			stringbuilder.append(separate).append("(").append(node.wakeTime).append(",")
+			.append(node.agent).append(")");
+			node = node.next;
+			separate = ";";
+		}
+		stringbuilder.append("]");
+		return (stringbuilder.toString());
 	}
 
 	@Override
-	public void enqueue(double wake, Agent agent) {
-		// TODO Auto-generated method stub
-		
+	public void enqueue(double waketime, Agent agent) throws IllegalArgumentException {
+		if (waketime < currentTime)
+			throw new IllegalArgumentException();
+		Node prevElement = head;
+		while ((prevElement.next != null) && (prevElement.next.wakeTime <= waketime)) {
+			prevElement = prevElement.next;
+		}
+		Node newElement = new Node(waketime, agent, prevElement.next);
+		prevElement.next = newElement;
+		size++;
 	}
 
+	Agent dequeue()
+	{
+		if (size < 1) 
+			throw new java.util.NoSuchElementException();
+		Agent rvAgent = head.next.agent;
+		head.next = head.next.next;
+		size--;
+		return rvAgent;
+	}
+
+	int size() {
+		return size;
+	}
+
+	boolean empty() {
+		return size() == 0;
+	}
+	
 	@Override
 	public void run(double duration) {
-		// TODO Auto-generated method stub
-		
+		double endtime = currentTime + duration;
+		while ((!empty()) && (head.next.wakeTime <= endtime)) {
+			currentTime = head.next.wakeTime;
+			dequeue().run(duration);
+			super.setChanged();
+			super.notifyObservers();
+		}
+		currentTime = endtime;
 	}
 }
