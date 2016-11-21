392ba915a733279cca3d743513422cfe19b73eac
derreck <mansheim23@hotmail.com>
2016-11-21 16:28:11 -0600

    fleshed out Light interface

--- TrafficSimulation/src/model/Light.java
+++ TrafficSimulation/src/model/Light.java
@@ -1,8 +1,25 @@
 package model;
 
-/**
- * A light has a boolean state.
- */
+
+import model.Data.LightState;
+
+public interface Light {
+	
+		public LightState getLightState(); 
+		public void setLightState(LightState state);
+		public boolean getState();
+		public void setState(boolean state);
+		public Long getGreenTimeNS();
+		public void setGreenTimeNS(Long greenTime);
+		public Long getGreenTimeEW();
+		public void setGreenTimeEW(Long greenTime);
+		public Long getYellowTimeNS();
+		public void setYellowTimeNS(Long yellowTime);
+		public Long getYellowTimeEW();
+		public void setYellowTimeEW(Long yellowTime);
+		
+}
+/*
 public class Light implements Agent {
 	Light() { } // Created only by this package
 
@@ -17,4 +34,4 @@ public class Light implements Agent {
 		}
 	}
 }
-
+*/
