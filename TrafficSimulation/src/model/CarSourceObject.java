5967bc60d3505eb67d20a31d0d0ee8f2463f5c5a
derreck <mansheim23@hotmail.com>
2016-11-21 15:05:59 -0600

    Added classes for light, intersection, sink, and car source object

---
+++ TrafficSimulation/src/model/CarSourceObject.java
@@ -0,0 +1,11 @@
+package model;
+
+import model.Data.Orientation;
+
+public class CarSourceObject extends CarSource {
+
+	public CarSourceObject(Orientation orientation) {
+		// TODO Auto-generated constructor stub
+	}
+
+}
---
+++ TrafficSimulation/src/model/IntersectionObject.java
@@ -0,0 +1,38 @@
+package model;
+
+import model.Data.Orientation;
+
+public class IntersectionObject implements RoadEnd {
+
+	@Override
+	public double getLastPosition() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+	@Override
+	public CarAcceptor getNextRoad(Orientation orientation) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public void remove(Car car) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public boolean accept(Car car, double d) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+	@Override
+	public double distanceToObstacle(double firstPosition,
+			Orientation orientation) {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
