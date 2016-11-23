bf52355151bd08eb9cd591c1698f48cb54f92bfe
derreck <mansheim23@hotmail.com>
2016-11-23 10:51:10 -0600

    made CarSourceObject final, added fields, constructor, and run method

--- TrafficSimulation/src/model/CarSourceObject.java
+++ TrafficSimulation/src/model/CarSourceObject.java
@@ -1,11 +1,46 @@
 package model;
 
+import properties.PropertyBag;
+import timeServer.TimeServer;
 import model.Data.Orientation;
 
-public class CarSourceObject extends CarSource {
-
-	public CarSourceObject(Orientation orientation) {
-		// TODO Auto-generated constructor stub
+final class CarSourceObject implements Agent, CarSource {
+	
+	private PropertyBag propertyBag = PropertyBag.makePropertyBag();
+	private Double generateCarDelay;
+	private CarAcceptor nextRoad;
+	private TimeServer time;
+	private Orientation orientation;
+	
+	CarSourceObject(Orientation orientation) {
+		this.time = this.propertyBag.getTimeServer();
+		this.orientation = orientation;
+		
+		this.generateCarDelay = Math.random() * propertyBag.getCarGenerationDelayMax();
+		this.generateCarDelay = Math.max(propertyBag.getCarGenerationDelayMin(), this.generateCarDelay);
+		
+		this.time.enqueue(this.time.currentTime(), this);
 	}
 
+	public void run(double time) {
+		Car c = Data.makeCar(orientation);
+		if (this.nextRoad == null) {
+			throw new NullPointerException("Next Road Was Not Set");
+		}
+		Boolean blocker = false;
+		for (Car e : this.nextRoad.getCars()) {
+			if (e.getFirstPosition() <= c.getCarLength() + c.getStopDistance()) {
+				blocker = true;
+			}
+		}
+		if (blocker == false) {
+			this.nextRoad.accept(c, 0.0);
+			this.time.enqueue(this.time.currentTime() + this.propertyBag.getTimeStep(), c);
+		}
+		this.time.enqueue(this.time.currentTime() + this.generateCarDelay, this);
+	}
+	
+	public void setNextRoad(CarAcceptor l) {
+		this.nextRoad = l;
+	}
 }
